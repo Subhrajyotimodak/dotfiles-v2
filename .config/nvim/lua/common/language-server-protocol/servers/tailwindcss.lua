5abb1dc736f942d344234d7c14104298ad99d220
Subhrajyoti Modak <46356816+Subhrajyotimodak@users.noreply.github.com>
2025-04-20 15:58:44 +0530

    changes

--- .config/nvim/lua/common/language-server-protocol/servers/tailwindcss.lua
+++ .config/nvim/lua/common/language-server-protocol/servers/tailwindcss.lua
@@ -5,6 +5,26 @@ if not status_ok then
 end
 
 return function(capabilities, on_attach)
+	local util = require("lspconfig.util")
+	local filetypes = {
+		"html",
+		"css",
+		"less",
+		"postcss",
+		"sass",
+		"scss",
+		"stylus",
+		"sugarss",
+		"javascript",
+		"javascriptreact",
+		"reason",
+		"rescript",
+		"typescript",
+		"typescriptreact",
+		"vue",
+		"svelte",
+	}
+
 	tailwindcss.setup(
 		---@type TailwindTools.Option
 		{
@@ -50,4 +70,59 @@ return function(capabilities, on_attach)
 			},
 		}
 	)
+
+	return {
+		cmd = { "tailwindcss-language-server", "--stdio" },
+		filetypes = filetypes,
+		on_attach = on_attach,
+		capabilities = capabilities,
+		on_new_config = function(new_config)
+			if not new_config.settings then
+				new_config.settings = {}
+			end
+			if not new_config.settings.editor then
+				new_config.settings.editor = {}
+			end
+			if not new_config.settings.editor.tabSize then
+				-- set tab size for hover
+				new_config.settings.editor.tabSize = vim.lsp.util.get_effective_tabstop()
+			end
+		end,
+		root_dir = function(fname)
+			return util.root_pattern(
+				"tailwind.config.js",
+				"tailwind.config.cjs",
+				"tailwind.config.mjs",
+				"tailwind.config.ts",
+				"postcss.config.js",
+				"postcss.config.cjs",
+				"postcss.config.mjs",
+				"postcss.config.ts"
+			)(fname) or vim.fs.dirname(vim.fs.find("package.json", { path = fname, upward = true })[1]) or vim.fs.dirname(
+				vim.fs.find("node_modules", { path = fname, upward = true })[1]
+			) or vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+		end,
+
+		settings = {
+			tailwindCSS = {
+				classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
+				includeLanguages = {
+					eelixir = "html-eex",
+					eruby = "erb",
+					htmlangular = "html",
+					templ = "html",
+				},
+				lint = {
+					cssConflict = "warning",
+					invalidApply = "error",
+					invalidConfigPath = "error",
+					invalidScreen = "error",
+					invalidTailwindDirective = "error",
+					invalidVariant = "error",
+					recommendedVariantOrder = "warning",
+				},
+				validate = true,
+			},
+		},
+	}
 end
